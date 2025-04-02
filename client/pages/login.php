9611a7ac54f09c0c0725ea97a47ac19f89272071
masterfully <daothanhtu.edu@gmail.com>
2025-04-02 13:21:28 +0700

    Update: giao diện login/register

--- client/pages/login.php
+++ client/pages/login.php
@@ -1,105 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <title>Login</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="../images/icons/favicon.ico" />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/util.css">
-    <link rel="stylesheet" type="text/css" href="../css/login.css">
-    <link rel="stylesheet" type="text/css" href="../css/signup.css">
-    <!--===============================================================================================-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Signup Form</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
-
 <body>
-
-    <div class="limiter">
-        <div class="container-login100">
-            <div class="wrap-login100">
-                <div class="login100-pic js-tilt" data-tilt>
-                    <img src="../images/img-01.png" alt="IMG">
+    <div class="container">
+        <div class="form-box login">
+            <form id="login-form" action="login?form=" method="POST" class="validate-form">
+                <h1>Login</h1>
+                <div class="input-box validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <input type="text" name="email" placeholder="Email" required>
+                    <i class='bx bxs-user'></i>
                 </div>
-                <!-- Form đăng nhập -->
-                <form id="login-form" class="login100-form validate-form" action="login?form=" method="POST">
-                    <span class="login100-form-title">
-                        Login
-                    </span>
+                <div class="input-box validate-input" data-validate="Password is required">
+                    <input name="pass" type="password" placeholder="Password" required>
+                    <i class='bx bxs-lock-alt' ></i>
+                </div>
+                <div class="forgot-link">
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+            </form>
+        </div>
 
-                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                        <input class="input100" type="text" name="email" placeholder="Email">
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100">
-                            <i class="fa fa-envelope" aria-hidden="true"></i>
-                        </span>
-                    </div>
+        <div class="form-box register">
+            <form id="register-form" action="register?form=" method="POST" class="validate-form">
+                <h1>Registration</h1>
+                <!-- <div class="input-box">
+                    <input type="text" placeholder="Username" required>
+                    <i class='bx bxs-user'></i>
+                </div> -->
+                <div class="input-box validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <input type="text" name="newEmail" placeholder="Email" required>
+                    <i class='bx bxs-envelope' ></i>
+                </div>
+                <div class="input-box validate-input" data-validate="Password is required">
+                    <input type="password" name="newPassword" placeholder="Password" required>
+                    <i class='bx bxs-lock-alt' ></i>
+                </div>
+                <button type="submit" class="btn">Register</button>
+            </form>
+        </div>
 
-                    <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <input class="input100" type="password" name="pass" placeholder="Password">
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                        </span>
-                    </div>
+        <div class="toggle-box">
+            <div class="toggle-panel toggle-left">
+                <h1>Hello, Welcome!</h1>
+                <p>Don't have an account?</p>
+                <button class="btn register-btn">Register</button>
+            </div>
 
-                    <div class="container-login100-form-btn">
-                        <button type="submit" class="login100-form-btn">
-                            Login
-                        </button>
-                    </div>
-
-                    <div class="text-center p-t-12">
-                        <span class="txt1">
-                            Forgot
-                        </span>
-                        <a class="txt2" href="#">
-                            Username / Password?
-                        </a>
-                    </div>
-
-                    <div class="text-center p-t-136" style="padding-top: 80px;">
-                        <a class="txt2" href="#" id="show-register">
-                            Create your Account
-                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                        </a>
-                    </div>
-
-                </form>
-
-                <!-- Form Đăng Ký (ẩn ban đầu) -->
-                <form id="register-form" class="login100-form validate-form" action="register?form=" method="POST" style="display: none;">
-                    <span class="login100-form-title">Create Account</span>
-
-                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                        <input class="input100" type="text" name="newEmail" placeholder="Email">
-                        <span class="symbol-input100"><i class="fa fa-envelope" aria-hidden="true"></i></span>
-                    </div>
-
-                    <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <input class="input100" type="password" name="newPassword" placeholder="Password">
-                        <span class="symbol-input100"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                    </div>
-
-                    <div class="container-signup-form-btn">
-                        <button type="submit" class="signup-form-btn">Register</button>
-                    </div>
-
-                    <div class="text-center p-t-12" style="padding-top: 55px;">
-                        <a class="txt2" href="#" id="show-login">Already have an account?</a>
-                    </div>
-                </form>
-
+            <div class="toggle-panel toggle-right">
+                <h1>Welcome Back!</h1>
+                <p>Already have an account?</p>
+                <button class="btn login-btn" id="show-login">Login</button>
             </div>
         </div>
     </div>
-
     <!--===============================================================================================-->
     <script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
     <!--===============================================================================================-->
@@ -119,5 +78,261 @@
     <script src="../js/auth.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bcryptjs/2.4.3/bcrypt.min.js"></script>
 </body>
-
 </html>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Poppins", sans-serif;
+        text-decoration: none;
+        list-style: none;
+    }
+
+    body{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background: linear-gradient(90deg, #e2e2e2, #c9d6ff);
+    }
+
+    .container{
+        position: relative;
+        width: 850px;
+        height: 550px;
+        background: #fff;
+        margin: 20px;
+        border-radius: 30px;
+        box-shadow: 0 0 30px rgba(0, 0, 0, .2);
+        overflow: hidden;
+    }
+
+        .container h1{
+            font-size: 36px;
+            margin: -10px 0;
+        }
+
+        .container p{
+            font-size: 14.5px;
+            margin: 15px 0;
+        }
+
+    form{ width: 100%; }
+
+    .form-box{
+        position: absolute;
+        right: 0;
+        width: 50%;
+        height: 100%;
+        background: #fff;
+        display: flex;
+        align-items: center;
+        color: #333;
+        text-align: center;
+        padding: 40px;
+        z-index: 1;
+        transition: .6s ease-in-out 1.2s, visibility 0s 1s;
+    }
+
+        .container.active .form-box{ right: 50%; }
+
+        .form-box.register{ visibility: hidden; }
+            .container.active .form-box.register{ visibility: visible; }
+
+    .input-box{
+        position: relative;
+        margin: 30px 0;
+    }
+
+        .input-box input{
+            width: 100%;
+            padding: 13px 50px 13px 20px;
+            background: #eee;
+            border-radius: 8px;
+            border: none;
+            outline: none;
+            font-size: 16px;
+            color: #333;
+            font-weight: 500;
+        }
+
+            .input-box input::placeholder{
+                color: #888;
+                font-weight: 400;
+            }
+        
+        .input-box i{
+            position: absolute;
+            right: 20px;
+            top: 50%;
+            transform: translateY(-50%);
+            font-size: 20px;
+        }
+
+    .forgot-link{ margin: -15px 0 15px; }
+        .forgot-link a{
+            font-size: 14.5px;
+            color: #333;
+        }
+
+    .btn{
+        width: 100%;
+        height: 48px;
+        background: #7494ec;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+        border: none;
+        cursor: pointer;
+        font-size: 16px;
+        color: #fff;
+        font-weight: 600;
+    }
+
+    .social-icons{
+        display: flex;
+        justify-content: center;
+    }
+
+        .social-icons a{
+            display: inline-flex;
+            padding: 10px;
+            border: 2px solid #ccc;
+            border-radius: 8px;
+            font-size: 24px;
+            color: #333;
+            margin: 0 8px;
+        }
+
+    .toggle-box{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+
+        .toggle-box::before{
+            content: '';
+            position: absolute;
+            left: -250%;
+            width: 300%;
+            height: 100%;
+            background: #7494ec;
+            /* border: 2px solid red; */
+            border-radius: 150px;
+            z-index: 2;
+            transition: 1.8s ease-in-out;
+        }
+
+            .container.active .toggle-box::before{ left: 50%; }
+
+    .toggle-panel{
+        position: absolute;
+        width: 50%;
+        height: 100%;
+        /* background: seagreen; */
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+        transition: .6s ease-in-out;
+    }
+
+        .toggle-panel.toggle-left{ 
+            left: 0;
+            transition-delay: 1.2s; 
+        }
+            .container.active .toggle-panel.toggle-left{
+                left: -50%;
+                transition-delay: .6s;
+            }
+
+        .toggle-panel.toggle-right{ 
+            right: -50%;
+            transition-delay: .6s;
+        }
+            .container.active .toggle-panel.toggle-right{
+                right: 0;
+                transition-delay: 1.2s;
+            }
+
+        .toggle-panel p{ margin-bottom: 20px; }
+
+        .toggle-panel .btn{
+            width: 160px;
+            height: 46px;
+            background: transparent;
+            border: 2px solid #fff;
+            box-shadow: none;
+        }
+
+    @media screen and (max-width: 650px){
+        .container{ height: calc(100vh - 40px); }
+
+        .form-box{
+            bottom: 0;
+            width: 100%;
+            height: 70%;
+        }
+
+            .container.active .form-box{
+                right: 0;
+                bottom: 30%;
+            }
+
+        .toggle-box::before{
+            left: 0;
+            top: -270%;
+            width: 100%;
+            height: 300%;
+            border-radius: 20vw;
+        }
+
+            .container.active .toggle-box::before{
+                left: 0;
+                top: 70%;
+            }
+
+            .container.active .toggle-panel.toggle-left{
+                left: 0;
+                top: -30%;
+            }
+
+        .toggle-panel{ 
+            width: 100%;
+            height: 30%;
+        }
+            .toggle-panel.toggle-left{ top: 0; }
+            .toggle-panel.toggle-right{
+                right: 0;
+                bottom: -30%;
+            }
+
+                .container.active .toggle-panel.toggle-right{ bottom: 0; }
+    }
+
+    @media screen and (max-width: 400px){
+        .form-box { padding: 20px; }
+
+        .toggle-panel h1{font-size: 30px; }
+    }
+
+</style>
+
+<script>
+    const container = document.querySelector('.container');
+    const registerBtn = document.querySelector('.register-btn');
+    const loginBtn = document.querySelector('.login-btn');
+
+    registerBtn.addEventListener('click', () => {
+        container.classList.add('active');
+    })
+
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove('active');
+    })
+</script>

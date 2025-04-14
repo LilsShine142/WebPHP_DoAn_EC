a08ca7890155e982e7c2029747e1ed31db3d9da1
masterfully <daothanhtu.edu@gmail.com>
2025-04-14 15:25:32 +0700

    chỉnh lại giao diện footer, show products

--- client/layouts/footer.php
+++ client/layouts/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg3 p-t-75 p-b-32">
 		<div class="container">
-			<div class="row">
+			<div class="row" style="justify-content: space-between;">
 				<div class="col-sm-6 col-lg-3 p-b-50">
 					<h4 class="stext-301 cl0 p-b-30">
 						Categories
